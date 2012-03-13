7b72ba06afedefe4b3b69322b3d7f5a83b4f4714
Walter Cacau <waltercacau@google.com>
2012-03-13 16:19:02 -0700

    Adding/creating libraries to support view testing

---
+++ light/src/test/webapp/jsTest/lightTest/RegisterFormViewTest.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+define(['light/RegisterFormView', 'light/RegisterFormController',
+        'dojo/query', 'lightTest/robot'],
+        function(RegisterFormView, RegisterFormController, query, robot) {
+  describe('light.RegisterFormView', function() {
+    var controller, view;
+    
+    beforeEach(function() {
+      this.stub(controller = new RegisterFormController());
+      view = new RegisterFormView();
+      view.setController(controller);
+      robot.attach(view);
+      document.body.appendChild(view.domNode);
+    });
+
+    describe('_button when clicked', function() {
+      it('should call controller.onSubmit', function() {
+        waitsFor(robot.click(view._button));
+        runs(function() {
+          expect(controller.onSubmit).toHaveBeenCalled();
+        });
+      });
+    });
+
+  });
+});
--- light/src/test/webapp/jsTest/loaderForJasmineMaven.js
+++ light/src/test/webapp/jsTest/loaderForJasmineMaven.js
@@ -97,12 +97,22 @@ var setupDojoLoaderForJasmine = function() {
     failed = true;
     debug = Array.prototype.slice.call(arguments, 0);
   });
+  /*require(['dojo/domReady!'], function() {
+    document.body.setAttribute('class', 'claro');
+  })*/
 };
 
+/*document.write(
+    '<link rel="stylesheet" type="text/css" '+
+    'href="src/main/webapp/js/external/djk/dijit/themes/claro/claro.css">');*/
+document.write(
+    '<script src="src/test/webapp/jsTest/external/syn.js"></script>');
 document.write(
     '<script src="src/test/webapp/jsTest/external/sinon-1.3.1.js"></script>');
 document.write(
     '<script src="src/test/webapp/jsTest/external/jasmine-sinon.js"></script>');
+document.write(
+    '<script src="src/test/webapp/jsTest/lightTestExtensions.js"></script>');
 document.write('<script src="src/test/webapp/jsTest/testConfig.js"></script>');
 document.write(
     '<script src="src/main/webapp/js/external/djk/dojo/dojo.js"></script>');
