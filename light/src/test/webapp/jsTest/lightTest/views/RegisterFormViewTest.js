20aa62fa76a627d7ae9dad0d9ceda012ea689cbd
Walter Cacau <waltercacau@google.com>
2012-03-13 19:43:17 -0700

    More tests for the RegisterFormView and JSDocs for robot

--- light/src/test/webapp/jsTest/lightTest/views/RegisterFormViewTest.js
+++ light/src/test/webapp/jsTest/lightTest/views/RegisterFormViewTest.js
@@ -13,18 +13,18 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-define(['light/views/RegisterFormView', 'light/controllers/RegisterFormController',
+define(['light/views/RegisterFormView',
+        'light/controllers/RegisterFormController',
         'dojo/query', 'lightTest/robot'],
         function(RegisterFormView, RegisterFormController, query, robot) {
   describe('light.views.RegisterFormView', function() {
     var controller, view;
-    
+
     beforeEach(function() {
       this.stub(controller = new RegisterFormController());
       view = new RegisterFormView();
       view.setController(controller);
       robot.attach(view);
-      document.body.appendChild(view.domNode);
     });
 
     describe('_button when clicked', function() {
@@ -32,6 +32,70 @@ define(['light/views/RegisterFormView', 'light/controllers/RegisterFormControlle
         waitsFor(robot.click(view._button));
         runs(function() {
           expect(controller.onSubmit).toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('_tosCheckbox initially', function() {
+      it('should not be checked', function() {
+        expect(view._tosCheckbox.get('checked')).toBeFalsy();
+      });
+    });
+
+    describe('_tosValidation', function() {
+      describe('when the _tosCheckbox is unchecked', function() {
+        it('should return false', function() {
+          view._tosCheckbox.set('checked', false);
+          expect(view._tosValidation()).toBeFalsy();
+        });
+      });
+      describe('when the _tosCheckbox is checked', function() {
+        it('should return true', function() {
+          view._tosCheckbox.set('checked', true);
+          expect(view._tosValidation()).toBeTruthy();
+        });
+      });
+    });
+
+    describe('', function() {
+      beforeEach(function() {
+        view._firstNameTextBox.set('value', 'FirstName');
+        view._lastNameTextBox.set('value', 'LastName');
+        view._tosCheckbox.set('checked', true);
+      });
+
+      describe('validate', function() {
+        describe('when all fields are correct', function() {
+          it('should return true', function() {
+            expect(view.validate()).toBeTruthy();
+          });
+        });
+        describe('when firstName is empty', function() {
+          it('should return false', function() {
+            view._firstNameTextBox.set('value', '');
+            expect(view.validate()).toBeFalsy();
+          });
+        });
+        describe('when lastName is empty', function() {
+          it('should return false', function() {
+            view._lastNameTextBox.set('value', '');
+            expect(view.validate()).toBeFalsy();
+          });
+        });
+        describe('when the _tosCheckbox is unchecke', function() {
+          it('should return false', function() {
+            view._tosCheckbox.set('checked', false);
+            expect(view.validate()).toBeFalsy();
+          });
+        });
+      });
+
+      describe('getData', function() {
+        it('should return the current filled data', function() {
+          expect(view.getData()).toEqual({
+            firstName: 'FirstName',
+            lastName: 'LastName'
+          });
         });
       });
     });
